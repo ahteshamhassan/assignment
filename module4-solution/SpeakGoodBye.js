0d85efa4e36a81c5ca4947371a3ffad334d11a93
aht-muz <64471927+aht-muz@users.noreply.github.com>
2020-05-19 13:56:03 +0400

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -1,5 +1,8 @@
+(function (window) {
 var byeSpeaker={};
-byeSpeaker.speakWord= "Good Bye";
+var speakWord= "Good Bye";
 byeSpeaker.speak=function(name){
-	console.log(byeSpeaker.speakWord +" "+ name)
-}
+	console.log(speakWord +" "+ name);
+};
+window.byeSpeaker=byeSpeaker;
+})(window);
