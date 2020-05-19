54983f0237bd439551c081c6d81655324a24a9d1
aht-muz <64471927+aht-muz@users.noreply.github.com>
2020-05-19 13:20:46 +0400

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,6 +1,6 @@
 (function (window) {
 
-var names = ["Athar", "Jon", "Ali","Jen","Paul","Laura","Jonathan","Jane"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i =0; i< names.length;  i++) {
 	var firstLetter= names[i].charAt(0).toLowerCase();
 	if (firstLetter==='j') {
